b0b1f05ce108d549ed003ef10029d7931b9e6560
Carlos Hernandez <learnercys@gmail.com>
2018-06-02 14:56:47 -0600

    Add vehicles endpoint to POST data
    Refactor when calling the NHTSA api.

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 var rp = require('request-promise');
 
-/* GET vehicles listing. */
-router.get('/:year/:manufacturer/:model', function(req, res, next) {
-  var { year, manufacturer, model } = req.params;
-
+function getVehicles(year, manufacturer, model) {
   var options = {
     uri: `https://one.nhtsa.gov/webapi/api/SafetyRatings/modelyear/${year}/make/${manufacturer}/model/${model}`,
     qs: {
@@ -13,14 +10,32 @@ router.get('/:year/:manufacturer/:model', function(req, res, next) {
     },
     json: true
   };
-  
-  rp(options)
+
+  return rp(options)
     .then(function(response) {
-      var vehicles = {
+      return {
         Count: response.Count,
         Results: response.Results
       }
+    });
+}
 
+router.post('/', function(req, res, next) {
+  var { modelYear, manufacturer, model } = req.body;
+
+  getVehicles(modelYear, manufacturer, model)
+    .then(function(vehicles) {
+      res.json(vehicles);
+    })
+    .catch(next);
+});
+
+/* GET vehicles listing. */
+router.get('/:year/:manufacturer/:model', function(req, res, next) {
+  var { year, manufacturer, model } = req.params;
+
+  getVehicles(year, manufacturer, model)
+    .then(function(vehicles) {
       res.json(vehicles);
     })
     .catch(next);
