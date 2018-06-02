fa76bb9016a51ed1cfa17c02274d13397853dc07
Carlos Hernandez <learnercys@gmail.com>
2018-06-02 12:57:17 -0600

    Add vehicles by filter endpoint
    The filters are:
    
    `year`: Vehicle's release year.
    `manufacturer`: Company that build the vehicle.
    `model`: Vehicle's model.

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
 
 /* GET vehicles listing. */
-router.get('/', function(req, res, next) {
-  res.json({ message: 'index'});
+router.get('/:year/:manufacturer/:model', function(req, res, next) {
+  var { year, manufacturer, model } = req.params;
+
+  var options = {
+    uri: `https://one.nhtsa.gov/webapi/api/SafetyRatings/modelyear/${year}/make/${manufacturer}/model/${model}`,
+    qs: {
+      format: 'json'
+    },
+    json: true
+  };
+  
+  rp(options)
+    .then(function(response) {
+      var vehicles = {
+        Count: response.Count,
+        Results: response.Results
+      }
+
+      res.json(vehicles);
+    })
+    .catch(next);
 });
 
 module.exports = router;
